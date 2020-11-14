63e28e4ae96f4d0582a661cc4d9e17b0f386acc4
ismail5g <ismail27.dec@gmail.com>
2020-11-15 00:03:52 +0600

    Added Entity & Interfaces

---
+++ DataSets/Interfaces/ICategory.cs
@@ -0,0 +1,9 @@
+﻿using DataSets.Entity;
+
+namespace DataSets.Interfaces
+{
+    public interface ICategory : IRepository<Category>
+    {
+        void Update(Category changeCategory);
+    }
+}
---
+++ DataSets/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DataSets.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategory Category { get; }
+        IProduct Product { get; }
+        void Save();
+    }
+}
