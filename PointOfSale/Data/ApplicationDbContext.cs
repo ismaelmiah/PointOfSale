bf8a9bb83dd06875f6d51e017a357a381f57097d
ismail5g <ismail27.dec@gmail.com>
2020-11-15 00:15:00 +0600

    Added Repository & UOW Patterns

--- PointOfSale/Data/ApplicationDbContext.cs
+++ PointOfSale/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DataSets.Entity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,8 @@ namespace PointOfSale.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
     }
 }
---
+++ PointOfSale/RepositoryPattern/Repository/CategoryRepo.cs
@@ -0,0 +1,27 @@
+﻿using DataSets.Entity;
+using DataSets.Interfaces;
+using PointOfSale.Data;
+using System.Linq;
+
+namespace PointOfSale.RepositoryPattern.Repository
+{
+    public class CategoryRepo : Repository<Category>, ICategory
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepo(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public void Update(Category category)
+        {
+            var data = _db.Categories.FirstOrDefault(x => x.Id == category.Id);
+            if (data == null) return;
+            data.Name = category.Name;
+            data.Description = category.Description;
+            data.ImageUrl = category.ImageUrl;
+            data.Products = category.Products;
+        }
+    }
+}
