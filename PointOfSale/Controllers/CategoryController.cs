8a85af51dd7ccd069bcf97b9731dd099d4b1a23a
ismail5g <ismail27.dec@gmail.com>
2020-11-16 21:16:21 +0600

    Category Crud Done

--- PointOfSale/Controllers/CategoryController.cs
+++ PointOfSale/Controllers/CategoryController.cs
@@ -1,5 +1,10 @@
-﻿using DataSets.Interfaces;
+﻿using System;
+using System.IO;
+using System.Linq;
+using DataSets.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using PointOfSale.Models;
 
 namespace PointOfSale.Controllers
@@ -7,9 +12,17 @@ namespace PointOfSale.Controllers
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _uow;
-        public CategoryController(IUnitOfWork uow)
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public CategoryController(IUnitOfWork uow, IWebHostEnvironment hostEnvironment)
         {
             _uow = uow;
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult GetAllData()
+        {
+            var allCategories = _uow.Category.GetAll(includeProperties: "Products");
+            return Json(new { data = allCategories });
         }
         public IActionResult Index()
         {
@@ -23,22 +36,57 @@ namespace PointOfSale.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(CategoryViewModel categoryvm)
+        public IActionResult Create(CategoryViewModel categoryViewModel)
         {
-            if (!ModelState.IsValid) return View(categoryvm);
-            _uow.Category.Add(categoryvm.category);
+            if (!ModelState.IsValid) return View(categoryViewModel);
+            _uow.Category.Add(categoryViewModel.category);
             _uow.Save();
             return RedirectToAction(nameof(Index));
         }
-        public IActionResult Edit()
+        public IActionResult Edit(Guid id)
         {
-            return View();
+            var model = _uow.Category.GetFirstOrDefault(x => x.Id == id);
 
+            var categoryViewModel = new CategoryViewModel()
+            {
+                category = model
+            };
+            return View(categoryViewModel);
         }
-        public IActionResult Delete()
+        [HttpPost]
+        public IActionResult Edit(CategoryViewModel categoryViewModel)
         {
-            return View();
+            if (!ModelState.IsValid) return View(categoryViewModel);
+            _uow.Category.Update(categoryViewModel.category);
+            _uow.Save();
+            return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Details(Guid id)
+        {
+            var model = _uow.Category.GetFirstOrDefault(x => x.Id == id, includeProperties: "Products");
+            return View(model);
+        }
+        [HttpDelete]
+        public IActionResult Delete(Guid id)
+        {
+            var deleteData = _uow.Category.GetFirstOrDefault(x => x.Id == id);
+            if (deleteData == null)
+                return Json(new { success = false, message = "Data Not Found!" });
+
+            if (deleteData.ImageUrl != null)
+            {
+                var webRootPath = _hostEnvironment.WebRootPath;
+                var imagePath = Path.Combine(webRootPath, deleteData.ImageUrl.TrimStart('\\'));
+
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+            }
+            _uow.Category.Remove(deleteData);
+            _uow.Save();
+            return Json(new { success = true, message = "Delete Operation Successfully" });
+        }
     }
 }
---
+++ PointOfSale/wwwroot/js/category.js
@@ -0,0 +1,70 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+
+function loadDataTable() {
+    dataTable = $('#myTable').DataTable({
+        "ajax": {
+            "url": "/Category/GetAllData"
+        },
+        "retrieve": true,
+        "columns": [
+            { "data": "name", "width": "10%" },
+            { "data": "description", "width": "20%" },
+            { "data": "products.length", "width": "15%" },
+            { "data": "description", "width": "20%" },
+            { "data": "products.length", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a onclick=Delete("/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i> 
+                                </a>
+                                <a href="/Category/Edit/${data}" class="btn btn-warning text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i> 
+                                </a>
+                                <a href="/Category/Details/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-info-circle"></i> 
+                                </a>
+                            </div>
+                           `;
+                }, "width": "20%"
+            }
+        ],
+        "language": {
+            "sLengthMenu": "Show _MENU_"
+        }
+    });
+}
+
+
+function Delete(url) {
+    swal({
+        title: 'Are you sure you want to Delete?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
